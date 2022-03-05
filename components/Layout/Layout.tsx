375eefb115d1bc471749d6d3f3b37ded855a6f39
Guido Queiroz <Takzzg@hotmail.com>
2022-03-05 02:18:09 -0300

    basic disclaimer

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,4 +1,5 @@
-import { ReactNode } from "react"
+import { ReactNode, useState } from "react"
+import { GrClose } from "react-icons/gr"
 
 import Navbar from "./Navbar/Navbar"
 import Footer from "./Footer/Footer"
@@ -10,12 +11,51 @@ interface Props {
 }
 
 const Layout = ({ children }: Props) => {
+    const [showDisclaimer, setShowDisclaimer] = useState(true)
+
+    const handleCloseDisclaimer = () => {
+        setShowDisclaimer(false)
+    }
+
+    const Disclaimer = () => {
+        return (
+            <div className={styles.disclaimerCont}>
+                <div className={styles.disclaimer}>
+                    <span className={styles.title}>Disclaimer</span>
+                    <span
+                        className={styles.close}
+                        onClick={handleCloseDisclaimer}
+                    >
+                        <GrClose />
+                    </span>
+                    <hr />
+                    <p className={styles.body}>
+                        This webpage is in early development; which means that
+                        some, if not most of the features, will not be working
+                        as intended or at all. Please consider reading the
+                        Readme.md from the{" "}
+                        <a href="https://github.com/Takzzg/portfolio">
+                            github repo
+                        </a>{" "}
+                        to learn more about the current state and development.
+                        <br />
+                        <br />
+                        Thanks for understanding, Guido.
+                    </p>
+                </div>
+            </div>
+        )
+    }
+
     return (
-        <div className={styles.layout}>
-            <Navbar />
-            {children}
-            <Footer />
-        </div>
+        <>
+            {showDisclaimer && <Disclaimer />}
+            <div className={styles.layout}>
+                <Navbar />
+                {children}
+                <Footer />
+            </div>
+        </>
     )
 }
 
